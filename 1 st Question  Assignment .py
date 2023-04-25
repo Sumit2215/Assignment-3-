a3ff09eeb47e0f9104fadd8fb1133ff157a78095
Sumit <131658103+Sumit2215@users.noreply.github.com>
2023-04-25 12:25:03 +0530

    Add files via upload

---
+++ 1 st Question  Assignment .py
@@ -0,0 +1,30 @@
+a= "Python is a case sensitive language"
+print(len(a))
+
+print(a[::-1])
+
+c=" case sensitive "[::1]
+
+d=a.replace("a case sensitive","object oriented")
+print(d)
+
+e="a"
+result=a.index(e)
+print("The index where a is found ",result)
+
+f=a.replace(" ","")
+print(f)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
